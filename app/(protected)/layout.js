aaadce388637e4ae3b7238ae5d80c1cbc4a26598
choijake0207 <jake.ch0712@gmail.com>
2025-01-27 14:58:12 -0500

    create base ui frame for private layout

--- app/(protected)/layout.js
+++ app/(protected)/layout.js
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from "../styles/protected.module.css"
 
-export default function ProtectedLayout() {
+export default function ProtectedLayout({children}) {
   return (
-    <div>
-      hello
+    <div className={styles.protected_layout}>
+      <nav className={styles.navbar}>
+        <Link href="/">Dashboard</Link>
+        <Link href="myhabits">My Habits</Link>
+        <Link href="friends">Friends</Link>
+        <Link href="/analytics">Analytics</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/settings">Settings</Link>
+      </nav>
+      <main className="protected-layout-outlets">{children}</main>
     </div>
   )
 }
