87ee5569659337d2963e528cec0e6cb67bbfef41
choijake0207 <jake.ch0712@gmail.com>
2025-01-28 13:58:11 -0500

    create register post request

--- app/component/LoginRegForm.js
+++ app/component/LoginRegForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import styles from "../styles/logreg.module.css"
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 const LoginRegForm = ({type}) => {
@@ -9,6 +10,32 @@ const LoginRegForm = ({type}) => {
     const [username, setUsername] = useState("")
     const [lastName, setLastName] = useState("")
     const [password, setPassword] = useState("")
+
+    const handleRegister = async(e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({firstName, lastName, username, password})
+            })
+            const result = response.json()
+            if (response.ok) {
+                console.log(result.message)
+                setFirstName("")
+                setLastName("")
+                setUsername("")
+                setPassword("")
+            } else {
+                console.log("error")
+            }
+           
+        } catch (error) {
+            console.error("Error submitting form:", error)
+        }
+    }
 
   return (
     type === "login" ? 
@@ -22,7 +49,7 @@ const LoginRegForm = ({type}) => {
             />
             <label>Password</label>
             <input
-                type="text"
+                type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
@@ -32,7 +59,7 @@ const LoginRegForm = ({type}) => {
             </p>
         </form>
     : 
-        <form  className={`${styles.logreg_form} ${styles.register_form}`}>
+        <form  className={`${styles.logreg_form} ${styles.register_form}`} onSubmit={handleRegister}>
             <label>First Name</label>
             <input
                 type="text"
@@ -53,7 +80,7 @@ const LoginRegForm = ({type}) => {
             />
             <label>Password</label>
             <input
-                type="text"
+                type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
