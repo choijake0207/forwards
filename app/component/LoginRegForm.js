eb45c85f2afb4ae40509cd37c3c711a9bdd9914b
choijake0207 <jake.ch0712@gmail.com>
2025-01-28 15:24:06 -0500

    import in auth methods from context

--- app/component/LoginRegForm.js
+++ app/component/LoginRegForm.js
@@ -1,10 +1,9 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import styles from "../styles/logreg.module.css"
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { login, register } from '../api/auth/AuthCalls'
-
+import { AuthContext } from '@/context/AuthContext'
 
 const LoginRegForm = ({type}) => {
 
@@ -13,6 +12,8 @@ const LoginRegForm = ({type}) => {
     const [lastName, setLastName] = useState("")
     const [password, setPassword] = useState("")
     const router = useRouter()
+    const {login, register} = useContext(AuthContext)
+
 
     const handleRegister = async(e) => {
         e.preventDefault()
@@ -43,57 +44,60 @@ const LoginRegForm = ({type}) => {
     }
 
   return (
-    type === "login" ? 
+    <>
+       
+        {type === "login" ? 
 
-        <form className={`${styles.logreg_form} ${styles.login_form}`} onSubmit={handleLogin}>
-            <label>Username</label>
-            <input 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label>Password</label>
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Login</button>
-            <p className={styles.logreg_form_message}>
-                Don't have an account? Click <Link href="/register">here</Link> to sign up
-            </p>
-        </form>
-    : 
-        <form  className={`${styles.logreg_form} ${styles.register_form}`} onSubmit={handleRegister}>
-            <label>First Name</label>
-            <input
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-            />
-            <label>Last Name</label>
-            <input
-                type="text"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-            <label>Username</label>
-            <input 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label>Password</label>
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Join</button>
-            <p className={styles.logreg_form_message}>
-                Already have an account? Click <Link href="/login">here</Link> to login
-            </p>
-        </form>
+            <form className={`${styles.logreg_form} ${styles.login_form}`} onSubmit={handleLogin}>
+                <label>Username</label>
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+                <p className={styles.logreg_form_message}>
+                    Don't have an account? Click <Link href="/register">here</Link> to sign up
+                </p>
+            </form>
+        : 
+            <form  className={`${styles.logreg_form} ${styles.register_form}`} onSubmit={handleRegister}>
+                <label>First Name</label>
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <label>Last Name</label>
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <label>Username</label>
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Join</button>
+                <p className={styles.logreg_form_message}>
+                    Already have an account? Click <Link href="/login">here</Link> to login
+                </p>
+            </form>}
+    </>
   )
 }
 
