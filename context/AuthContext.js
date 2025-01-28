d97f285c6bd435b91fca99346dfab3d303428ab3
choijake0207 <jake.ch0712@gmail.com>
2025-01-28 15:23:26 -0500

    create authentication context for auth state / add all auth api calls

---
+++ context/AuthContext.js
@@ -0,0 +1,58 @@
+"use client"
+import {createContext, useState, useEffect} from "react"
+
+export const AuthContext = createContext()
+export const AuthProvider = ({children}) => {
+    const [authUser, setAuthUser] = useState({
+        id: "",
+        firstName: "",
+        username: "",
+        status: false
+    })
+
+    const login = async (username, password) => {
+        try {
+            const response = await fetch("/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({username, password})
+            })
+            if (!response.ok) {
+                const errorData = await response.json()
+                throw new Error(errorData.error)
+            }
+            return await response.json()
+        } catch (error) {
+            console.error("Error logging in:", error)
+            throw error
+        }
+    }
+
+    const register = async(firstName, lastName, username, password) => {
+        try {
+            const response = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({firstName, lastName, username, password})
+            })
+            if (!response.ok) {
+                const errorData = await response.json()
+                throw new Error(errorData.error)
+            
+            }
+            return await response.json()
+        } catch (error) {
+            console.error("Error submitting form:", error)
+            throw error
+        }
+    }
+    return (
+        <AuthContext.Provider value={{authUser, login, register}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
