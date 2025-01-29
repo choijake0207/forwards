775fc22db3bec08deaa6466ed326dd764827891a
choijake0207 <jake.ch0712@gmail.com>
2025-01-29 10:36:39 -0500

    add active class to link based on path

--- app/(protected)/layout.js
+++ app/(protected)/layout.js
@@ -3,13 +3,14 @@ import Link from 'next/link'
 import React, { useContext, useEffect } from 'react'
 import styles from "../styles/protected.module.css"
 import { AuthContext } from '@/context/AuthContext'
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import ClientLoading from '../component/ClientLoading'
 import { SquaresFour, Users, GearSix, User, SignOut, House, ChartPie   } from 'phosphor-react'
 
 export default function ProtectedLayout({children}) {
   const {authUser, loading} = useContext(AuthContext)
   const router = useRouter()
+  const pathname = usePathname()
   useEffect(() => {
     if (!authUser.status && !loading) {
       router.push("/login")
@@ -21,12 +22,12 @@ export default function ProtectedLayout({children}) {
     <div className={styles.protected_layout}>
       <aside className={styles.protected_aside}>
         <nav className={styles.navbar}>
-          <Link href="/"><House/>Dashboard</Link>
-          <Link href="myhabits"><SquaresFour/>My Habits</Link>
-          <Link href="friends"><Users/>Friends</Link>
-          <Link href="/analytics"><ChartPie/>Analytics</Link>
-          <Link href="/profile"><User/>Profile</Link>
-          <Link href="/settings"><GearSix/>Settings</Link>
+          <Link href="/" className={pathname === "/" ? "active" : ""}><House/>Dashboard</Link>
+          <Link href="myhabits" className={pathname === "/myhabits" ? "active" : ""}><SquaresFour/>My Habits</Link>
+          <Link href="friends" className={pathname === "/friends" ? "active" : ""}><Users/>Friends</Link>
+          <Link href="/analytics" className={pathname === "/analytics" ? "active" : ""}><ChartPie/>Analytics</Link>
+          <Link href="/profile" className={pathname === "/profile" ? "active" : ""}><User/>Profile</Link>
+          <Link href="/settings" className={pathname === "/settings" ? "active" : ""}><GearSix/>Settings</Link>
         </nav>
       </aside>
       <main className="protected-layout-outlets">
