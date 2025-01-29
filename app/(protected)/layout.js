52fe3b22a3938aefd10473d042fafdfa27c23f38
choijake0207 <jake.ch0712@gmail.com>
2025-01-29 11:02:38 -0500

    add logout button event handling

--- app/(protected)/layout.js
+++ app/(protected)/layout.js
@@ -8,7 +8,7 @@ import ClientLoading from '../component/ClientLoading'
 import { SquaresFour, Users, GearSix, User, SignOut, House, ChartPie   } from 'phosphor-react'
 
 export default function ProtectedLayout({children}) {
-  const {authUser, loading} = useContext(AuthContext)
+  const {authUser, loading, logout} = useContext(AuthContext)
   const router = useRouter()
   const pathname = usePathname()
   useEffect(() => {
@@ -22,17 +22,20 @@ export default function ProtectedLayout({children}) {
     <div className={styles.protected_layout}>
       <aside className={styles.protected_aside}>
         <nav className={styles.navbar}>
-          <Link href="/" className={pathname === "/" ? "active" : ""}><House/>Dashboard</Link>
-          <Link href="myhabits" className={pathname === "/myhabits" ? "active" : ""}><SquaresFour/>My Habits</Link>
-          <Link href="friends" className={pathname === "/friends" ? "active" : ""}><Users/>Friends</Link>
-          <Link href="/analytics" className={pathname === "/analytics" ? "active" : ""}><ChartPie/>Analytics</Link>
-          <Link href="/profile" className={pathname === "/profile" ? "active" : ""}><User/>Profile</Link>
-          <Link href="/settings" className={pathname === "/settings" ? "active" : ""}><GearSix/>Settings</Link>
+          <Link href="/" className={pathname === "/" ? `${styles.active}` : ""}><House/>Dashboard</Link>
+          <Link href="myhabits" className={pathname === "/myhabits" ? `${styles.active}` : ""}><SquaresFour/>My Habits</Link>
+          <Link href="friends" className={pathname === "/friends" ? `${styles.active}` : ""}><Users/>Friends</Link>
+          <Link href="/analytics" className={pathname === "/analytics" ? `${styles.active}` : ""}><ChartPie/>Analytics</Link>
+          <Link href="/profile" className={pathname === "/profile" ? `${styles.active}` : ""}><User/>Profile</Link>
+          <Link href="/settings" className={pathname === "/settings" ? `${styles.active}` : ""}><GearSix/>Settings</Link>
         </nav>
+        <button className={styles.sign_out_btn} onClick={logout}>
+          <SignOut/>
+          Sign Out
+        </button>
       </aside>
       <main className="protected-layout-outlets">
         {loading ? <ClientLoading/> : children}
-
       </main>
     </div>
   )
