f1bb05a17a2bd152614a3c796299cfd3b41e97ae
choijake0207 <jake.ch0712@gmail.com>
2025-01-30 18:12:14 -0500

    add useEffect wrapper for fetchhabits api

--- app/(protected)/myhabits/page.js
+++ app/(protected)/myhabits/page.js
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client"
+import React, {useState, useEffect} from 'react'
+import styles from "../../styles/myHabits.module.css"
+import { fetchHabitsAPI } from '@/app/api/protected/habit/HabitCalls'
 
-const page = () => {
+
+export default function MyHabits() {
+  const [habits, setHabits] = useState([])
+  useEffect(() => {
+    const fetchHabits = async () => {
+      try {
+        const response = await fetchHabitsAPI()
+        setHabits(response)
+        console.log(response)
+      } catch (error) {
+        console.error("error fetching habits", error)
+      }
+    }
+    fetchHabits()
+  }, [])
+
+
   return (
-    <div>
+    <div className={styles.my_habits_page}>
+
       
     </div>
   )
 }
 
-export default page
