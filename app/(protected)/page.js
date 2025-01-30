81cc1c41da1269be71218e9d6db4c0ed54a1e808
choijake0207 <jake.ch0712@gmail.com>
2025-01-30 12:51:49 -0500

    add form toggling

--- app/(protected)/page.js
+++ app/(protected)/page.js
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client"
+import React, {useContext, useState} from 'react'
+import { AuthContext } from '@/context/AuthContext'
+import HabitForm from '../component/HabitForm'
 
 const page = () => {
+  const {authUser} = useContext(AuthContext)
+  const [formVisible, setFormVisible] = useState(false)
   return (
     <div>
-      
+      <h1>Welcome To Dashboard {authUser.firstName}</h1>
+      <button onClick={() => setFormVisible(!formVisible)}>Form</button>
+      {formVisible && <HabitForm onClose={() => setFormVisible(false)}/>}
     </div>
   )
 }
