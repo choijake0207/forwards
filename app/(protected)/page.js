985cf5e0483822763f84a546f7e34b9a16b90acf
choijake0207 <jake.ch0712@gmail.com>
2025-01-31 21:17:27 -0500

    import in progress and checklist widget components // set up loading states

--- app/(protected)/page.js
+++ app/(protected)/page.js
@@ -3,6 +3,11 @@ import React, {useContext, useState, useEffect} from 'react'
 import { AuthContext } from '@/context/AuthContext'
 import HabitForm from '../component/HabitForm'
 import styles from "../styles/dashboard.module.css"
+import { fetchHabitsAPI } from '@/app/api/protected/habit/HabitCalls'
+import ClientLoading from '@/app/component/ClientLoading'
+import CheckListWidget from '../component/dashboard/CheckListWidget'
+import ProgressWidget from '../component/dashboard/ProgressWidget'
+
 
 
 
@@ -29,7 +34,15 @@ export default function Dashboard () {
   }, [])
   return (
     <div className={`${styles.dashboard_page} page`}>
-      <h1>Welcome To Dashboard {authUser.firstName}</h1>
+      <header>
+        <h1>Here's Your Dashboard, {authUser.firstName}</h1>
+      </header>
+      <div className={styles.dashboard_main_content}>
+        <ProgressWidget/>
+        <aside className={styles.dashboard_side_widgets_container}>
+          {habits ? <CheckListWidget habits={habits}/> : <ClientLoading/>}
+        </aside>
+      </div>
       <button onClick={() => setFormVisible(!formVisible)}>Form</button>
       {formVisible && <HabitForm onClose={() => setFormVisible(false)}/>}
     </div>
