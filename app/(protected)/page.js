6dc313fd8483a13b9277ccb7ec94294c6bd7d092
choijake0207 <jake.ch0712@gmail.com>
2025-01-31 20:39:34 -0500

    move fetch function from myhabits to dash

--- app/(protected)/page.js
+++ app/(protected)/page.js
@@ -1,13 +1,34 @@
 "use client"
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { AuthContext } from '@/context/AuthContext'
 import HabitForm from '../component/HabitForm'
+import styles from "../styles/dashboard.module.css"
 
-const page = () => {
+
+
+
+
+export default function Dashboard () {
   const {authUser} = useContext(AuthContext)
   const [formVisible, setFormVisible] = useState(false)
+  const [habits, setHabits] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const fetchHabits = async () => {
+      try {
+        const response = await fetchHabitsAPI()
+        setHabits(response)
+        console.log(response)
+      } catch (error) {
+        console.error("error fetching habits", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchHabits()
+  }, [])
   return (
-    <div>
+    <div className={`${styles.dashboard_page} page`}>
       <h1>Welcome To Dashboard {authUser.firstName}</h1>
       <button onClick={() => setFormVisible(!formVisible)}>Form</button>
       {formVisible && <HabitForm onClose={() => setFormVisible(false)}/>}
@@ -15,4 +36,4 @@ const page = () => {
   )
 }
 
-export default page
+
