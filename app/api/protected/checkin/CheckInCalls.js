19461010575c85fdff1a34fbe28347ebae5618c3
choijake0207 <jake.ch0712@gmail.com>
2025-02-03 11:31:28 -0500

    create api call for undoing checkin

--- app/api/protected/checkin/CheckInCalls.js
+++ app/api/protected/checkin/CheckInCalls.js
@@ -23,3 +23,27 @@ export async function createCheckInAPI ({habitId}) {
         throw console.error()
     }
 }
+
+//delete/undo checkin
+export async function deleteCheckInAPI ({habitId}) {
+    try {
+        const token = localStorage.getItem("token")
+        const response = await fetch("/api/protected/checkin/delete", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({habitId})
+        })
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(errorData.error)
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error("Error undoing check in", error)
+        throw console.error()
+    }
+}
