dd23a62d88f55017cfcbf1b3fb6ec9a9f756493c
choijake0207 <jake.ch0712@gmail.com>
2025-02-02 20:14:16 -0500

    add modal button

--- app/component/habit/card.js
+++ app/component/habit/card.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import styles from "../../styles/habit.module.css"
-import { XCircle, CheckCircle } from 'phosphor-react'
-export default function Card({name, type, color, createdAt, status, id}) {
+import styles from "../../styles/habitCard.module.css"
+import { XCircle, CheckCircle, DotsThreeVertical } from 'phosphor-react'
+export default function Card({name, type, color, id}) {
   return (
     <article className={`${styles.habit_card} ${styles[color]}`}>
         <div className={styles.card_header}>
             {type === "START" ? <CheckCircle/> : <XCircle/>}
-            <h1 className={styles.card_title}>{name}</h1>
+            <p className={styles.card_title}>{name}</p>
+            <button className={styles.modal_btn}>
+              <DotsThreeVertical/>
+            </button>
         </div>
+        <button className={styles.check_in_btn}>Check In</button>
     </article>
   )
 }
