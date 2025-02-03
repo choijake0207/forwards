ece62099a43f1244c91d4a558173dd4c6f849c3a
choijake0207 <jake.ch0712@gmail.com>
2025-02-03 16:03:21 -0500

    render grid or bar based off display state'

--- app/component/habit/ProgressCard.js
+++ app/component/habit/ProgressCard.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from "../../styles/progressCard.module.css"
+import ProgressBar from './ProgressBar'
+import ProgressGrid from './ProgressGrid'
 
-export default function ProgressCard({habit, windowType, displayType}) {
+
+export default function ProgressCard({habit, windowType, displayType, dayCount}) {
   return (
     <div className={styles.progress_card}>
         <p>{habit.name}</p>
+        {
+            displayType === "grid" ?
+            <ProgressGrid habit={habit} windowType={windowType} dayCount={dayCount}/>
+            : <ProgressBar/>
+        }
     </div>
   )
 }
