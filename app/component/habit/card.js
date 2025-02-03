34c137b89884acf3f1172bbba6acfce30b98386e
choijake0207 <jake.ch0712@gmail.com>
2025-02-02 22:12:20 -0500

    add boolean check to see if habits been checked in

--- app/component/habit/card.js
+++ app/component/habit/card.js
@@ -2,10 +2,13 @@ import React from 'react'
 import styles from "../../styles/habitCard.module.css"
 import { XCircle, CheckCircle, DotsThreeVertical } from 'phosphor-react'
 
-export default function Card({name, type, color, id, handleCheckIn}) {
+export default function Card({name, type, color, id, lastCheck, handleCheckIn}) {
+  const today = new Date().setHours(0, 0, 0, 0)
+  let checked = lastCheck && new Date(lastCheck).setHours(0, 0, 0, 0) === today
+  console.log(checked)
 
   return (
-    <article className={`${styles.habit_card} ${styles[color]}`}>
+    <article className={`${styles.habit_card} ${styles[color]} ${checked ? styles.completed : ""}`}>
         <div className={styles.card_header}>
             {type === "START" ? <CheckCircle/> : <XCircle/>}
             <p className={styles.card_title}>{name}</p>
