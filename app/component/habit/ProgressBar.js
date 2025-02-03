d9ac3a90d81c94994dc9a2dcccd66b5ac52c8376
choijake0207 <jake.ch0712@gmail.com>
2025-02-03 16:02:52 -0500

    create progress display type components

---
+++ app/component/habit/ProgressBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "../../styles/progressCard.module.css"
+export default function ProgressBar({window, habit}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ app/component/habit/ProgressGrid.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/progressCard.module.css"
+export default function ProgressGrid({windowType, habit, dayCount}) {
+    console.log(dayCount)
+  return (
+    <div className={styles.progress_grid}>
+      
+    </div>
+  )
+}
