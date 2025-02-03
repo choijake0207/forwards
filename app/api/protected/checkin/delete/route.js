031104664df0b6ae399505207e9423aa501935ff
choijake0207 <jake.ch0712@gmail.com>
2025-02-03 11:26:41 -0500

    create route handler for undoing checkins

---
+++ app/api/protected/checkin/delete/route.js
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken"
+
+export async function DELETE (request) {
+    try {
+        const authHeader = request.headers.get("Authorization") 
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            console.log("Missing or Invalid Token")
+            return NextResponse.json({error: "Missing or Invalid Token"})
+        }
+        const token = authHeader.split(" ")[1]
+        let verified
+        try {
+            verified = jwt.verify(token, process.env.JWT_SECRET)
+            console.log("token verified")
+        } catch (error) {
+            console.error("JWT verification failed", error)
+            return NextResponse.json({error: "Invalid Token"})
+        }
+        const {habitId} = await request.json()
+        const today = new Date().setHours(0, 0, 0, 0)
+        const normalizedToday = new Date(today) 
+        const prisma = new PrismaClient()
+        const target = await prisma.checkIn.findUnique({
+            where: {
+                habitId: habitId,
+                userId: verified.id,
+                date: normalizedToday
+            }
+        })
+        await prisma.checkIn.delete({
+            where: {
+                id: target.id
+            }
+        })
+        await prisma.habit.update({
+            where: {
+                id: habitId
+            },
+            data: {
+                lastCheck: null
+            }
+        })
+    } catch (error) {
+        console.error("Failed To Undo Check In", error)
+        return NextResponse.json({error: "Failed To Undo Check In"}, {status: 500})
+    }
+}
