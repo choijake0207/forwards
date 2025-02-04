908ee57f3c344033b863b3142f2c69d1756e8d66
choijake0207 <jake.ch0712@gmail.com>
2025-02-03 19:22:30 -0500

    generate progress grid based on timeframe and check in booleans

--- app/component/habit/ProgressGrid.js
+++ app/component/habit/ProgressGrid.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import styles from "../../styles/progressCard.module.css"
-export default function ProgressGrid({windowType, habit, dayCount}) {
-    console.log(dayCount)
+export default function ProgressGrid({windowType, habit, days}) {
+
   return (
     <div className={styles.progress_grid}>
+        {
+            days.map(day => {
+                return (
+                    <div
+                        className={`${day.isChecked ? styles.checked : ""} ${day.isCheckInDay ? styles.checkin_day :""} ${styles.grid_cell}`}
+                    >
+                    </div>
+                )
+            })
+        }
       
     </div>
   )
