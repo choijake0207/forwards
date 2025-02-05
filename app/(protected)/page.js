d0788a1f704b823e538efb17b32ac301f9fce431
choijake0207 <jake.ch0712@gmail.com>
2025-02-05 11:20:23 -0500

    move all habit related functions into context for easier shared state

--- app/(protected)/page.js
+++ app/(protected)/page.js
@@ -1,157 +1,44 @@
 "use client"
 import React, {useContext, useState, useEffect, useMemo} from 'react'
 import { AuthContext } from '@/context/AuthContext'
+import { HabitContext } from '@/context/HabitContext'
 import HabitForm from '../component/HabitForm'
 import styles from "../styles/dashboard.module.css"
-import { fetchHabitsAPI } from '@/app/api/protected/habit/HabitCalls'
 import ClientLoading from '@/app/component/ClientLoading'
 import CheckListWidget from '../component/dashboard/CheckListWidget'
 import ProgressWidget from '../component/dashboard/ProgressWidget'
-import { createCheckInAPI, deleteCheckInAPI } from '../api/protected/checkin/CheckInCalls'
-import { startOfWeek, endOfWeek, startOfMonth, endOfMonth, getDaysInMonth, format, differenceInDays, getTime, addWeeks, addMonths  } from 'date-fns'
+import {format} from 'date-fns'
 
 
 
 export default function Dashboard () {
   const {authUser} = useContext(AuthContext)
-  const [habits, setHabits] = useState(null)
-  const [loading, setLoading] = useState(true)
-
-  // SHARED MODIFIED HABITS STATE
-  const [updatedHabits, setUpdatedHabits] = useState([])
-
-  // PROGRESS COMPONENT STATES
-  const [progressWindow, setProgressWindow] = useState("Week")
-  const [windowOffset, setWindowOffset] = useState(0)
+  const {
+    progressWindow, 
+    setProgressWindow, 
+    setWindowOffset, 
+    windowOffset, 
+    getTimeFrame, 
+    checkIn, 
+    undoCheck,
+    processedHabits,
+    loading
+  } = useContext(HabitContext)
   const [formVisible, setFormVisible] = useState(false)
 
-// INITIAL ALL HABITS FETCH
-  useEffect(() => {
-    const fetchHabits = async () => {
-      try {
-        const response = await fetchHabitsAPI()
-        setHabits(response)
-      } catch (error) {
-        console.error("error fetching habits", error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchHabits()
-  }, [])
-
-  // PROGRESS COMPONENT FNs
-
-    // set time frame
-  const getTimeFrame = useMemo(() => {
-    const today = new Date()
-    if (!habits || habits.length === 0) {
-      return {start: today, end: today}
-    }
-    if (progressWindow === "Week") {
-      return {start: startOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1}), end: endOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1})}
-    } else if (progressWindow === "Month") {
-      return {start: startOfMonth(addMonths(today, windowOffset)), end: endOfMonth(addMonths(today, windowOffset))}
-    } else {
-      const firstHabitDate = habits.length > 0 ? new Date(Math.min(...habits.map(habit => new Date(habit.createdAt)))) : today
-      return {start: firstHabitDate, end: today}
-    }
-  }, [progressWindow, habits, windowOffset])
-
-    // format time frame window
+  // FORMAT TIME FRAME FOR UI
   const formatTimeFrame = (start, end) => {
     return `${format(start, "MM/dd")} - ${format(end, "MM/dd")}`
   }
-    // create days object per habit
-  const generateDayObjects = (habit) => {
-    let current = new Date(getTimeFrame.start)
-    let days = []
-    while (current <= getTimeFrame.end) {
-      let stringFormat = current.toLocaleString("en-US", {weekday: "short"})
-      let formattedCurrent = format(current, "yyyy-MM-dd")
-      // is today a check in day (boolean)
-      const isCheckInDay = habit.frequency === "DAILY" || habit.daysOfWeek.includes(stringFormat)
-      // has today been checked (boolean)
-      const isChecked = habit.checkIns.some(checkIn => format(new Date(checkIn.date).setHours(0,0,0,0), "yyyy-MM-dd") === formattedCurrent)
-      // append booleans and date as object into days array
-      days.push({
-        date: current,
-        isCheckInDay,
-        isChecked
-      })
-      current = new Date(current.getTime() + 86400000)
-    }
-    return days
-  }
-    // attach day object to each habit
-    useEffect(() => {
-      if (habits && habits.length > 0) {
-      setUpdatedHabits(
-        habits ? habits.map(habit => ({
-          ...habit,
-          days: generateDayObjects(habit)
-        })) : []
-      );
-    }
-    }, [habits, getTimeFrame]);
-
 
   // CHECKLIST COMPONENT FILTERING
   const date = new Date()
   const today = date.toLocaleString("en-US", {weekday: "short"})
   const todayHabits = useMemo(() => {
-    if (!updatedHabits) return []
-    return updatedHabits.filter(habit => habit.frequency === "DAILY" || habit.daysOfWeek.includes(today))
-  }, [updatedHabits, today])
+    if (!processedHabits) return []
+    return processedHabits.filter(habit => habit.frequency === "DAILY" || habit.daysOfWeek.includes(today))
+  }, [processedHabits, today])
  
-
-
-  // CREATE CHECKIN
-  const checkIn = async (habitId) => {
-    const previousHabits = [...updatedHabits]
-    try {
-      //optimistic render
-      setUpdatedHabits(prev => 
-        prev.map(habit => 
-          habit.id === habitId ? 
-            {...habit, 
-              lastCheck: new Date(), 
-              checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}],
-              days: generateDayObjects({...habit, checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}]})
-            } 
-            : habit
-        )
-      )
-      const response = await createCheckInAPI({habitId})
-    } catch (error) {
-      console.error("Error Checking In", error)
-      // revert optimistic render
-      setUpdatedHabits(previousHabits)
-    }
-  }
-
-  // DELETE CHECKIN
-  const undoCheck = async (habitId) => {
-    const previousHabits = [...updatedHabits]
-    const today = new Date(new Date().setHours(0,0,0,0))
-    try {
-      setUpdatedHabits(prev => 
-        prev.map(habit => 
-          habit.id === habitId ? 
-            {...habit, 
-              lastCheck: null,
-              checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd")),
-              days: generateDayObjects({...habit, checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd"))})
-            } 
-            : habit
-        )  
-      )
-      const response = await deleteCheckInAPI({habitId})
-    } catch (error) {
-        console.error("Error Undoing Check", error)
-        setUpdatedHabits(previousHabits)
-    }
-  }
 
   return (
     <div className={`${styles.dashboard_page} page`}>
@@ -166,7 +53,7 @@ export default function Dashboard () {
             ? <ClientLoading/> 
             : <ProgressWidget 
                 toggleForm={() => setFormVisible(true)} 
-                updatedHabits={updatedHabits}
+                habits={processedHabits}
                 handleWindowChange={(type) => setProgressWindow(type)}
                 handleDecrement={() => setWindowOffset(prev => prev - 1)}
                 handleIncrement={() => setWindowOffset(prev => prev + 1)}
---
+++ context/HabitContext.js
@@ -0,0 +1,134 @@
+"use client"
+import { createContext, useState, useEffect, useMemo } from "react";
+import { startOfWeek, endOfWeek, startOfMonth, endOfMonth, getDaysInMonth, format, differenceInDays, getTime, addWeeks, addMonths  } from 'date-fns'
+import { fetchHabitsAPI } from "@/app/api/protected/habit/HabitCalls";
+import { createCheckInAPI, deleteCheckInAPI } from "@/app/api/protected/checkin/CheckInCalls";
+
+export const HabitContext = createContext()
+export const HabitProvider = ({children}) => {
+    const [rawHabits, setRawHabits] = useState(null)
+    const [processedHabits, setProcessedHabits] = useState([])
+    const [progressWindow, setProgressWindow] = useState("Week")
+    const [windowOffset, setWindowOffset] = useState(0)
+    const [loading, setLoading] = useState(true)
+
+
+ // INITIAL ALL HABITS FETCH
+    useEffect(() => {
+        const fetchHabits = async () => {
+        try {
+            const response = await fetchHabitsAPI()
+            setRawHabits(response)
+        } catch (error) {
+            console.error("error fetching habits", error)
+        } finally {
+            setLoading(false)
+        }
+        }
+        fetchHabits()
+    }, [])
+
+  // TIMEFRAME CALCULATOR (User Selected)
+    const getTimeFrame = useMemo(() => {
+        const today = new Date()
+        if (!rawHabits || rawHabits.length === 0) {
+            return {start: today, end: today}
+        }
+        if (progressWindow === "Week") {
+            return {start: startOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1}), end: endOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1})}
+        } else if (progressWindow === "Month") {
+            return {start: startOfMonth(addMonths(today, windowOffset)), end: endOfMonth(addMonths(today, windowOffset))}
+        } else {
+            const firstHabitDate = rawHabits.length > 0 ? new Date(Math.min(...rawHabits.map(habit => new Date(habit.createdAt)))) : today
+            return {start: firstHabitDate, end: today}
+        }
+    }, [progressWindow, rawHabits, windowOffset])
+
+    const generateDayObjects = (habit) => {
+        let current = new Date(getTimeFrame.start)
+        let days = []
+        while (current <= getTimeFrame.end) {
+        let stringFormat = current.toLocaleString("en-US", {weekday: "short"})
+        let formattedCurrent = format(current, "yyyy-MM-dd")
+        // is today a check in day (boolean)
+        const isCheckInDay = habit.frequency === "DAILY" || habit.daysOfWeek.includes(stringFormat)
+        // has today been checked (boolean)
+        const isChecked = habit.checkIns.some(checkIn => format(new Date(checkIn.date).setHours(0,0,0,0), "yyyy-MM-dd") === formattedCurrent)
+        // append booleans and date as object into days array
+        days.push({
+            date: current,
+            isCheckInDay,
+            isChecked
+        })
+        current = new Date(current.getTime() + 86400000)
+        }
+        return days
+    }
+
+ // PROCESS HABITS BY APPENDING DAYS ARRAY BASED ON TF
+    useEffect(() => {
+        if (rawHabits && rawHabits.length > 0) {
+            setProcessedHabits(
+                rawHabits ? rawHabits.map(habit => ({
+                    ...habit,
+                    days: generateDayObjects(habit)
+                })) : []
+            );
+        }
+    }, [rawHabits, getTimeFrame]);
+
+ // CREATE CHECKIN
+    const checkIn = async (habitId) => {
+        const previousHabits = [...processedHabits]
+        try {
+          //optimistic render
+          setProcessedHabits(prev => 
+            prev.map(habit => 
+              habit.id === habitId ? 
+                {...habit, 
+                  lastCheck: new Date(), 
+                  checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}],
+                  days: generateDayObjects({...habit, checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}]})
+                } 
+                : habit
+            )
+          )
+          const response = await createCheckInAPI({habitId})
+        } catch (error) {
+          console.error("Error Checking In", error)
+          // revert optimistic render
+          setProcessedHabits(previousHabits)
+        }
+    }
+
+      // DELETE CHECKIN
+    const undoCheck = async (habitId) => {
+        const previousHabits = [...processedHabits]
+        const today = new Date(new Date().setHours(0,0,0,0))
+        try {
+        setProcessedHabits(prev => 
+            prev.map(habit => 
+            habit.id === habitId ? 
+                {...habit, 
+                lastCheck: null,
+                checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd")),
+                days: generateDayObjects({...habit, checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd"))})
+                } 
+                : habit
+            )  
+        )
+        const response = await deleteCheckInAPI({habitId})
+        } catch (error) {
+            console.error("Error Undoing Check", error)
+            setProcessedHabits(previousHabits)
+        }
+    }
+
+    return (
+        <HabitContext.Provider value={{loading, undoCheck, checkIn, processedHabits, windowOffset, setWindowOffset, progressWindow, setProgressWindow, getTimeFrame}}>
+            {children}
+        </HabitContext.Provider>
+    )
+
+
+}
