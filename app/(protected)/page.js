74c03c2bdb7bb50dfc9cc94ea2adf1031b0843d8
choijake0207 <jake.ch0712@gmail.com>
2025-02-04 22:59:34 -0500

    fix date formatting in check in optimistic render functions

--- app/(protected)/page.js
+++ app/(protected)/page.js
@@ -48,15 +48,15 @@ export default function Dashboard () {
     if (!habits || habits.length === 0) {
       return {start: today, end: today}
     }
-    if (window === "Week") {
+    if (progressWindow === "Week") {
       return {start: startOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1}), end: endOfWeek(addWeeks(today, windowOffset), {weekStartsOn: 1})}
-    } else if (window === "Month") {
+    } else if (progressWindow === "Month") {
       return {start: startOfMonth(addMonths(today, windowOffset)), end: endOfMonth(addMonths(today, windowOffset))}
     } else {
       const firstHabitDate = habits.length > 0 ? new Date(Math.min(...habits.map(habit => new Date(habit.createdAt)))) : today
       return {start: firstHabitDate, end: today}
     }
-  }, [window, habits, windowOffset])
+  }, [progressWindow, habits, windowOffset])
 
     // format time frame window
   const formatTimeFrame = (start, end) => {
@@ -69,11 +69,10 @@ export default function Dashboard () {
     while (current <= getTimeFrame.end) {
       let stringFormat = current.toLocaleString("en-US", {weekday: "short"})
       let formattedCurrent = format(current, "yyyy-MM-dd")
-      console.log("today format", formattedCurrent)
       // is today a check in day (boolean)
       const isCheckInDay = habit.frequency === "DAILY" || habit.daysOfWeek.includes(stringFormat)
       // has today been checked (boolean)
-      const isChecked = habit.checkIns.some(checkIn => format(new Date(checkIn.date), "yyyy-MM-dd") === formattedCurrent)
+      const isChecked = habit.checkIns.some(checkIn => format(new Date(checkIn.date).setHours(0,0,0,0), "yyyy-MM-dd") === formattedCurrent)
       // append booleans and date as object into days array
       days.push({
         date: current,
@@ -86,12 +85,14 @@ export default function Dashboard () {
   }
     // attach day object to each habit
     useEffect(() => {
+      if (habits && habits.length > 0) {
       setUpdatedHabits(
         habits ? habits.map(habit => ({
           ...habit,
           days: generateDayObjects(habit)
         })) : []
       );
+    }
     }, [habits, getTimeFrame]);
 
 
@@ -104,6 +105,7 @@ export default function Dashboard () {
   }, [updatedHabits, today])
  
 
+
   // CREATE CHECKIN
   const checkIn = async (habitId) => {
     const previousHabits = [...updatedHabits]
@@ -114,14 +116,13 @@ export default function Dashboard () {
           habit.id === habitId ? 
             {...habit, 
               lastCheck: new Date(), 
-              checkIns: [...habit.checkIns, {date: format(new Date(), "yyyy-MM-dd")}],
-              days: generateDayObjects({...habit, checkIns: [...habit.checkIns, {date: format(new Date(), "yyyy-MM-dd")}]})
+              checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}],
+              days: generateDayObjects({...habit, checkIns: [...habit.checkIns, {date: new Date(new Date().setHours(0,0,0,0))}]})
             } 
             : habit
         )
       )
       const response = await createCheckInAPI({habitId})
-      console.log(response)
     } catch (error) {
       console.error("Error Checking In", error)
       // revert optimistic render
@@ -132,20 +133,20 @@ export default function Dashboard () {
   // DELETE CHECKIN
   const undoCheck = async (habitId) => {
     const previousHabits = [...updatedHabits]
+    const today = new Date(new Date().setHours(0,0,0,0))
     try {
       setUpdatedHabits(prev => 
         prev.map(habit => 
           habit.id === habitId ? 
             {...habit, 
               lastCheck: null,
-              checkIns: habit.checkIns.filter(checkIn => checkIn.date !== format(new Date(), "yyyy-MM-dd")),
-              days: generateDayObjects({...habit, checkIns: habit.checkIns.filter(checkIn => checkIn.date !== format(new Date(), "yyyy-MM-dd"))})
+              checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd")),
+              days: generateDayObjects({...habit, checkIns: habit.checkIns.filter(checkIn => format(checkIn.date, "yyyy-MM-dd") !== format(today, "yyyy-MM-dd"))})
             } 
             : habit
         )  
       )
       const response = await deleteCheckInAPI({habitId})
-      console.log(response)
     } catch (error) {
         console.error("Error Undoing Check", error)
         setUpdatedHabits(previousHabits)
@@ -170,6 +171,7 @@ export default function Dashboard () {
                 handleDecrement={() => setWindowOffset(prev => prev - 1)}
                 handleIncrement={() => setWindowOffset(prev => prev + 1)}
                 formattedWindow={formatTimeFrame(getTimeFrame.start, getTimeFrame.end)}
+                window={progressWindow}
               />
           }
         </div>
