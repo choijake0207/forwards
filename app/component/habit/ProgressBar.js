6f4c487a1a881e9dce963cb4df961fc9fcea47d5
choijake0207 <jake.ch0712@gmail.com>
2025-02-06 15:36:08 -0500

    create progress bar component

--- app/component/habit/ProgressBar.js
+++ app/component/habit/ProgressBar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from "../../styles/progressCard.module.css"
-export default function ProgressBar({window, habit}) {
+export default function ProgressBar({ days, color}) {
+
+  // bar progress = days checked / total days to be checked
+  let daysToBeChecked = days.filter(day => day.isCheckInDay === true)
+  let daysChecked = days.filter(day => day.isChecked === true)
+  let progress = (daysChecked.length / daysToBeChecked.length) * 100
+
+
   return (
-    <div>
-      
+    <div className={styles.progress_bar}>
+      <div className={`${styles.progress_bar_filler} ${styles[color]}`} style={{width: `${progress}%`}}></div>
     </div>
   )
 }
+ 
