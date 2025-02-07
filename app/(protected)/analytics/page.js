82b7452870538c9987dfc80b32b7b5d106dbd3f9
choijake0207 <jake.ch0712@gmail.com>
2025-02-07 12:40:22 -0500

    start adding analytics widget components

--- app/(protected)/analytics/page.js
+++ app/(protected)/analytics/page.js
@@ -1,11 +1,33 @@
-import React from 'react'
+"use client"
+import React, {useContext, useMemo} from 'react'
+import styles from "../../styles/analytics.module.css"
+import { HabitContext } from '@/context/HabitContext'
+export default function Analytics() {
 
-const page = () => {
+  const {processedHabits} = useContext(HabitContext)
+  
+  // Total # of checkins
+  let totalCheckIns = processedHabits.flatMap(habit => habit.checkIns)
+  console.log(totalCheckIns)
+  
+  // Total # of missed checkins (i.e isChecked === false && isCheckInDay === true)
+  
+
+
+
   return (
-    <div>
+    <div className={`${styles.analytics} ${styles.page}`}>
+      <div className={styles.graph_component}>GRAPH COMPONENT</div>
+      <section className={styles.analytics_widget_row}>
+
+        <div className={styles.analytics_widget}>
+          <p>Total Check Ins:</p>
+          <p>{totalCheckIns.length}</p>
+        </div>
+
+      </section>
+      <ul className={styles.analytics_accordion}>Accordion</ul>
       
     </div>
   )
 }
-
-export default page
