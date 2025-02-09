a20056648deebd1b71035176158340e5095caf28
choijake0207 <jake.ch0712@gmail.com>
2025-02-08 21:08:51 -0500

    Create habit delete api call

--- app/api/protected/habit/HabitCalls.js
+++ app/api/protected/habit/HabitCalls.js
@@ -46,6 +46,28 @@ export async function fetchHabitsAPI() {
         throw console.error()
     }
 }
+
+export async function deleteHabitAPI () {
+    try {
+        const token = localStorage.getItem("token")
+        const response = await fetch("/api/protected/habit/delete", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(errorData.error)
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error("Failed To Delete Habit", error)
+        throw console.error()
+    }
+}
 // Fetch Single Habit By Id (considering just filtering from habitContext)
 // export async function fetchSingleHabitAPI(habitId) {
 //     try {
