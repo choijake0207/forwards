fd093a4d1c5a617c4c12f22484df62a75102faf1
choijake0207 <jake.ch0712@gmail.com>
2025-02-09 16:42:10 -0500

    replace icons

--- app/component/habit/ProgressCard.js
+++ app/component/habit/ProgressCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 import styles from "../../styles/progressCard.module.css"
 import ProgressBar from './ProgressBar'
 import ProgressGrid from './ProgressGrid'
-import { CheckCircle, XCircle } from 'phosphor-react'
+import { Check, X } from 'phosphor-react'
 
 
 export default function ProgressCard({habit, displayType, days}) {
   return (
     <div className={styles.progress_card}>
         <p className={styles[habit.color]}>
-          {habit.type === "START" ? <CheckCircle/> : <XCircle/>}
+          {habit.type === "START" ? <Check/> : <X/>}
           {habit.name}
         </p>
         {
