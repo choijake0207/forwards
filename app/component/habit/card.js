acda1682d34c32b55f283ad65701fa603e3f4ff7
choijake0207 <jake.ch0712@gmail.com>
2025-02-11 14:58:26 -0500

    add class for whether today is checkinday

--- app/component/habit/card.js
+++ app/component/habit/card.js
@@ -1,13 +1,13 @@
 import React, {useContext, useRef, useState, useEffect} from 'react'
 import styles from "../../styles/habitCard.module.css"
-import { XCircle, CheckCircle, DotsThreeVertical, ArrowCounterClockwise } from 'phosphor-react'
+import { XCircle, CheckCircle, DotsThreeVertical, ArrowCounterClockwise, Trash, Wrench } from 'phosphor-react'
 import { HabitContext } from '@/context/HabitContext'
 
-export default function Card({name, type, color, id, isChecked}) {
-  const {undoCheck, checkIn} = useContext(HabitContext)
+export default function Card({name, type, color, id, isChecked, isToday}) {
+  const {undoCheck, checkIn, deleteHabit} = useContext(HabitContext)
   const [modalOpen, setModalOpen] = useState(false)
   return (
-    <article className={`${styles.habit_card} ${styles[color]} ${isChecked ? styles.completed : styles.incomplete}`}>
+    <article className={`${styles.habit_card} ${styles[color]} ${isChecked ? styles.completed : styles.incomplete} ${isToday ? styles.active : styles.inactive}`}>
         <div className={styles.card_header}>
             {type === "START" ? <CheckCircle/> : <XCircle/>}
             <p className={styles.card_title}>{name}</p>
@@ -20,13 +20,13 @@ export default function Card({name, type, color, id, isChecked}) {
           : <button className={styles.check_in_btn}  onClick={() => checkIn(id)}>Check In</button> 
         }
         {
-          modalOpen && <CardModal isOpen={modalOpen} onClose={() => setModalOpen(false)}/>
+          modalOpen && <CardModal isOpen={modalOpen} onClose={() => setModalOpen(false)} deleteHabit={deleteHabit} id={id}/>
         }
     </article>
   )
 }
 
-function CardModal ({isOpen, onClose}) {
+function CardModal ({isOpen, onClose, deleteHabit, id}) {
   const modalRef = useRef(null)
   useEffect(() => {
     function handleOutsideClick (event) {
@@ -46,8 +46,8 @@ function CardModal ({isOpen, onClose}) {
   }, [isOpen, modalRef, onClose])
   return (
     <div className={styles.card_modal} ref={modalRef}>
-      <button>Edit Habit</button>
-      <button>Delete Habit</button>
+      <button className={styles.edit_btn}><Wrench/>Edit Habit</button>
+      <button className={styles.delete_btn} onClick={() => deleteHabit(id)}><Trash/>Delete Habit</button>
     </div>
 
   )
