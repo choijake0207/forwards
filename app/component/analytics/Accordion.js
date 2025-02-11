92fded1f2e5d2b69b2eac49ce3d6afc980fcbef0
choijake0207 <jake.ch0712@gmail.com>
2025-02-11 17:27:26 -0500

    create base accordion card layout and accordion

---
+++ app/component/analytics/Accordion.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import styles from "../../styles/analytics.module.css"
+
+export default function Accordion({habits}) {
+    
+
+  return (
+    <ul className={styles.analytics_accordion}>
+        {
+            habits.map(habit =>  {
+                return (
+                    <li key={habit.id} className={styles.accordion_card}>
+                        <p>{habit.name}</p>
+                        <div className={styles.data_table}>
+                            <div className={styles.streaks_data}>
+                                <p className={styles.data_label}>Longest Streak:</p>
+                                <p className={styles.data_display}>
+                                    Coming Soon
+                                </p>
+                            </div>
+                            <div className={styles.checkCt_data}>
+                                <p className={styles.data_label}>Total check ins:</p>
+                                <p className={styles.data_display}>{habit.checkIns.length}</p>
+                            </div>
+                            <div className={styles.checkRt_data}>
+                                <p className={styles.data_label}>Check in rate:</p>
+                                <p className={styles.data_display}>{Math.round((habit.checkIns.length / habit.days.length) * 100)}%</p>
+                            </div>
+                            <div className={styles.start_data}>
+                                <p className={styles.data_label}>Days Since Start:</p>
+                                <p className={styles.data_display}>fuck my avgina</p>
+                            </div>
+                            <div className={styles.freq_data}>
+                                <p className={styles.data_label}>Frequency:</p>
+                                <p className={styles.data_display}>{habit.frequency}</p>
+                            </div>
+                            <div className={styles.dow_data}>
+                                <p className={styles.data_label}>Days of the week:</p>
+                                {habit.daysOfWeek.map(day => { return (<p>{day}</p>)})}
+                            </div>
+                        </div>
+                    </li>
+                )
+            })
+        }
+    </ul>
+  )
+}
+
+
