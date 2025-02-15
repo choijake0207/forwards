201bc3501f6546f553598291b61a2e706a77d51c
choijake0207 <jake.ch0712@gmail.com>
2025-02-15 09:53:29 -0800

    import rate generator from graph, calculate avg rate

--- app/(protected)/analytics/page.js
+++ app/(protected)/analytics/page.js
@@ -8,6 +8,7 @@ import Accordion from '@/app/component/analytics/Accordion'
 import ClientLoading from '@/app/component/ClientLoading'
 import { Wrench } from "phosphor-react"
 export default function Analytics() {
+
   const {rawHabits, loading, optimisticCheckIns} = useContext(HabitContext)
 
   // get date of first habit creation (starting point for graph component)
@@ -15,6 +16,8 @@ export default function Analytics() {
     if (!rawHabits || rawHabits.length === 0) return new Date()
     return new Date(Math.min(...rawHabits.map(habit => new Date(habit.createdAt))))
   }, [rawHabits])
+
+
 
   // generate days array
   const generateAnalyticDays = (habit) => {
@@ -46,6 +49,8 @@ export default function Analytics() {
     return days
   }
 
+
+  // append days to habits
   const analyticsHabits = useMemo(() => {
     if (!rawHabits || rawHabits.length === 0) return []
     return rawHabits.map(habit => ({
@@ -55,9 +60,46 @@ export default function Analytics() {
   }, [rawHabits])
     
   
-   // Widget Data 
+
+    // total check ins
    let totalCheckIns = []
    analyticsHabits.map(habit => habit.checkIns.forEach(checkIn => {totalCheckIns.push(checkIn)}))
+
+
+
+   // check in rate calculator
+   const start = new Date(firstHabitDate).setHours(0,0,0,0)
+   const today = new Date().setHours(0,0,0,0)
+
+   const dataSetGenerator = useMemo(() => {
+    let current = new Date(start)
+    let rate = []
+    while (current <= today) {
+      let formattedDate = new Date(new Date(current).setHours(0,0,0,0))
+      let totalCheckInDays = 0
+      let totalCheckIns = 0
+
+      analyticsHabits.forEach(habit => {
+        const dayObject = habit.days.find(day => format(new Date(day.date).setHours(0,0,0,0), "yyyy-MM-dd") == format(new Date(formattedDate).setHours(0,0,0,0), "yyyy-MM-dd"))
+        if (dayObject) {
+          if (dayObject.isCheckInDay) totalCheckInDays++
+          if (dayObject.isChecked) totalCheckIns++
+        }
+      })
+
+      let dailyRate = totalCheckInDays > 0 ? (totalCheckIns / totalCheckInDays) * 100 : 0
+      rate.push(dailyRate)
+      current.setDate(current.getDate() + 1)
+    }
+    return rate
+  }, [start, today, analyticsHabits])
+
+
+  // avg check in rate calculator
+  let rateSum = dataSetGenerator.reduce((a, b) => a + b, 0)
+  let avg = rateSum/dataSetGenerator.length
+
+
 
 
   return (
@@ -69,7 +111,7 @@ export default function Analytics() {
         <section className={styles.graph_and_accordion}>
           <div className={styles.graph_container}>
             {
-              loading ? <ClientLoading/> : <Graph habits={analyticsHabits} firstDate={firstHabitDate}/>
+              loading ? <ClientLoading/> : <Graph habits={analyticsHabits} firstDate={firstHabitDate} rate={dataSetGenerator}/>
             }
           </div>
           <Accordion habits={analyticsHabits}/>
@@ -78,20 +120,20 @@ export default function Analytics() {
         <aside className={styles.side_components}>
           <div className={styles.widgets_container}>
             <div className={styles.total_check_widget}>
-              <p className={styles.widget_label}>Total Check Ins:</p>
-              <p className={styles.widget_data}>{totalCheckIns.length}</p>
+              <p className={styles.widget_label}>Total Habits:</p>
+              {!loading && <p className={styles.widget_data}>{rawHabits.length}</p>}
             </div>
             <div className={styles.total_check_widget}>
               <p className={styles.widget_label}>Total Check Ins:</p>
               <p className={styles.widget_data}>{totalCheckIns.length}</p>
             </div> 
             <div className={styles.total_check_widget}>
-              <p className={styles.widget_label}>Total Check Ins:</p>
-              <p className={styles.widget_data}>{totalCheckIns.length}</p>
+              <p className={styles.widget_label}>Avg. Check In Rate:</p>
+              <p className={styles.widget_data}>{avg}</p>
             </div>
             <div className={styles.total_check_widget}>
-              <p className={styles.widget_label}>Total Check Ins:</p>
-              <p className={styles.widget_data}>{totalCheckIns.length}</p>
+              <p className={styles.widget_label}>Longest Streak:</p>
+              <p className={styles.widget_data}>-</p>
             </div>
           </div>
           <div className={styles.updates_container}>
