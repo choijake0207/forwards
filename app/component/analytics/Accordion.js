8c1f031474e0b19b6be92dbaafbe37f0564c61da
choijake0207 <jake.ch0712@gmail.com>
2025-02-15 11:54:16 -0800

    format data table displays

--- app/component/analytics/Accordion.js
+++ app/component/analytics/Accordion.js
@@ -30,31 +30,31 @@ export default function Accordion({habits}) {
                             </div>
 
                             <div className={`${styles.data_table}`}>
-                                <div className={styles.streaks_data}>
-                                    <p className={styles.data_label}>Longest Streak:</p>
-                                    <p className={styles.data_display}>
-                                        Coming Soon
-                                    </p>
-                                </div>
                                 <div className={styles.checkCt_data}>
-                                    <p className={styles.data_label}>Total check ins:</p>
+                                    <p className={styles.data_label}>Check Ins:</p>
                                     <p className={styles.data_display}>{habit.checkIns.length}</p>
                                 </div>
                                 <div className={styles.checkRt_data}>
-                                    <p className={styles.data_label}>Check in rate:</p>
+                                    <p className={styles.data_label}>Rate:</p>
                                     <p className={styles.data_display}>{Math.round((habit.checkIns.length / habit.days.length) * 100)}%</p>
                                 </div>
                                 <div className={styles.start_data}>
                                     <p className={styles.data_label}>Days Since Start:</p>
-                                    <p className={styles.data_display}>fuck my avgina</p>
+                                    <p className={styles.data_display}>{Math.round((new Date() - new Date(habit.createdAt))/86400000)}</p>
                                 </div>
                                 <div className={styles.freq_data}>
                                     <p className={styles.data_label}>Frequency:</p>
-                                    <p className={styles.data_display}>{habit.frequency}</p>
+                                    <p className={styles.data_display}>{habit.frequency.charAt(0) + habit.frequency.slice(1).toLowerCase()}</p>
                                 </div>
                                 <div className={styles.dow_data}>
-                                    <p className={styles.data_label}>Days of the week:</p>
-                                    {habit.daysOfWeek.map(day => { return (<p key={day}>{day}</p>)})}
+                                    <p className={styles.data_label}>Days:</p>
+                                    <p className={styles.data_display}>
+                                        {habit.frequency === "DAILY" ? "Everyday" 
+                                            : habit.daysOfWeek.length === 1
+                                                ? habit.daysOfWeek[0]
+                                                : habit.daysOfWeek.join(", ")
+                                        }
+                                    </p>
                                 </div>
                                 
                             </div>
