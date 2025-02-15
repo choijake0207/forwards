25f0372b1d6a0baca16e4f918de286ad32fb232d
choijake0207 <jake.ch0712@gmail.com>
2025-02-15 12:53:04 -0800

    add hover modal for cell date

--- app/component/habit/ProgressGrid.js
+++ app/component/habit/ProgressGrid.js
@@ -1,28 +1,40 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import styles from "../../styles/progressCard.module.css"
 import { HabitContext } from '@/context/HabitContext'
+import {format} from "date-fns"
 
 export default function ProgressGrid({days, color, habit}) {
     const {progressWindow} = useContext(HabitContext)
+    const [dateModal, setDateModal] = useState(null)
 
   return (
-    <div className={`${styles.progress_grid } ${styles[progressWindow]}`}>
+    <div className={`${styles.progress_grid } ${styles[progressWindow]}`} key={habit.id}>
  
         {
-            days.map(day => {
+            days.map((day, index) => {
                 return (
-                    <div
-                        key={day.date}
-                        className={`
-                            ${day.isChecked ? styles.checked : ""} 
-                            ${day.isCheckInDay ? styles.checkin_day :""} 
-                            ${styles.grid_cell}  
-                            ${styles[color]}
-                            ${new Date(day.date).getTime() < new Date(habit.createdAt).setHours(0,0,0,0) ? styles.nonexist : ""}
+            
+                        <div
+                            key={day.date}
+                            className={`
+                                ${day.isChecked ? styles.checked : ""} 
+                                ${day.isCheckInDay ? styles.checkin_day :""} 
+                                ${styles.grid_cell}  
+                                ${styles[color]}
+                                ${new Date(day.date).getTime() < new Date(habit.createdAt).setHours(0,0,0,0) ? styles.nonexist : ""}
 
-                        `}
-                    >
-                    </div>
+                            `}
+                            onMouseOver={() => setDateModal(index)}
+                            onMouseOut={() => setDateModal(null)}
+                        >
+                            {dateModal === index && 
+                                <div key={index} id={styles.cell_modal}>
+                                    {format(day.date, "M/d")}
+                                </div>
+                            }
+                        </div>
+                      
+       
                 )
             })
         }
