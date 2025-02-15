43f9e3f50b737dec33183562985f9ae6c4546661
choijake0207 <jake.ch0712@gmail.com>
2025-02-14 19:00:01 -0800

    add changepassword api call

--- app/api/protected/user/UserCalls.js
+++ app/api/protected/user/UserCalls.js
@@ -20,3 +20,27 @@ export async function fetchUserAPI() {
     }
 }
 
+
+export async function changePasswordAPI(oldPassword, newPassword) {
+    try {
+        const token = localStorage.getItem("token")
+        const response = await fetch("/api/protected/user/edit", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({oldPassword, newPassword})
+        })
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(errorData.error)
+        }
+        const data = await response.json()
+        return data
+        
+    } catch (error) {
+        console.error("Error Changing Password", error)
+        throw error
+    }
+}
