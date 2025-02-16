40648cceef50e71500f40cc59f06149dba906511
choijake0207 <jake.ch0712@gmail.com>
2025-02-15 19:04:14 -0800

    create custom loader with ldrs library

--- app/component/ClientLoading.js
+++ app/component/ClientLoading.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import styles from "../styles/loader.module.css"
+import {ring2} from 'ldrs'
 
-const ClientLoading = () => {
+const ClientLoading = ({type}) => {
+  ring2.register()
   return (
-    <div>
-        <p>...Loading....</p>
-      
+    <div className={`${styles.client_loader} ${styles[type]}`}>
+        <l-ring-2 size="40" stroke="4" color={"rgb(52, 114, 231)"}></l-ring-2>
     </div>
   )
 }
