3cf3b0e28b0206c69d906dfa5123a859adf5f417
choijake0207 <jake.ch0712@gmail.com>
2025-02-17 16:05:49 -0800

    create custom loader component

--- app/component/ClientLoading.js
+++ app/component/ClientLoading.js
@@ -1,13 +1,17 @@
+"use client"
 import React from 'react'
 import styles from "../styles/loader.module.css"
-import {ring2} from 'ldrs'
+
 
 const ClientLoading = ({type}) => {
-  ring2.register()
+
   return (
     <div className={`${styles.client_loader} ${styles[type]}`}>
-        <l-ring-2 size="40" stroke="4" color={"rgb(52, 114, 231)"}></l-ring-2>
+      <div className={styles.loader_circle}>
+        <div className={styles.loader_progress}></div>
+      </div>
     </div>
+
   )
 }
 
