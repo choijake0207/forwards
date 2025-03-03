c627f8f42dd5582424609de4f0b4c043d2564c5d
choijake0207 <jake.ch0712@gmail.com>
2025-03-03 10:55:00 -0800

    create landing page

---
+++ app/(public)/landing/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/landing.module.css"
+
+export default function Landing() {
+  return (
+    <div className={styles.landing_page}>
+      This is Landing
+    </div>
+  )
+}
